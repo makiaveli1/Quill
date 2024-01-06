86ce7442b2705eb90695f749a4f97e925164c1b1
makiaveli1 <itsmakiaveli@gmail.com>
2024-01-06 16:23:02 +0000

    customized admin and updated fixtures with unique slugs

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import Product, Category, Author
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Display the Category model fields in the Admin view
+    """
+    list_display = ("name", "friendly_name", "slug")
+    search_fields = ("name", "friendly_name")
+    prepopulated_fields = {"slug": ("friendly_name",)}
+
+
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Display the Product model fields in the Admin view
+    """
+    list_display = (
+        "name",
+        "rating",
+        "price",
+        "category",
+        "author",
+        "format",
+        "book_depository_stars",
+        "currency",
+        "old_price",
+        "isbn",
+        "slug",
+        "image_url"
+    )
+    search_fields = ("name", "author__name", "category__name")
+    list_filter = ("category", "author", "rating", "format")
+    prepopulated_fields = {"slug": ("name",)}
+    ordering = ("-rating",)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    """
+    Display the Author model fields in the Admin view
+    """
+    list_display = ("name", "slug")
+    search_fields = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Author, AuthorAdmin)
