ab19a9a6f665ed98547d028c93984b2f69197a92
makiaveli1 <itsmakiaveli@gmail.com>
2024-03-10 17:33:42 +0000

    set Debug false

--- quill/settings.py
+++ quill/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-!%afc554mw#k5(11juwv-t##dd-a#!3bya$8go30(wmzqb3vt9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['quill-7g6f.onrender.com', '127.0.0.1', 'localhost']
 USE_CRISPY_BOOTSTRAP4 = True
