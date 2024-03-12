f5b6c3edb806716960e7ac4ac0790de2cfecc409
makiaveli1 <itsmakiaveli@gmail.com>
2024-03-12 16:12:32 +0000

    final fixes to email setup

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,19 +1,22 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import OrderLineItem
-
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
-    """
-    Update Order total on lineitem -
-    """
     instance.order.update_total()
-
+    # Send email after saving if necessary
+    subject = 'Your Order Update'
+    message = render_to_string('email/order_update_email.txt', {'order': instance.order})
+    send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [instance.order.email])
 
 @receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
-    """
-    Update order total on lineitem delete
-    """
     instance.order.update_total()
+    # Send email after deletion if necessary
+    subject = 'Your Order Update'
+    message = render_to_string('email/order_update_email.txt', {'order': instance.order})
+    send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [instance.order.email])
--- quill/settings.py
+++ quill/settings.py
@@ -217,9 +217,9 @@ ADMINS = [('Gbemi', 'likwidtv@gmail.com')]
 
 # stripe
 STRIPE_CURRENCY = 'usd'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
-STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET')
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
