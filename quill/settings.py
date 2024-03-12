36f33c2fd8c849dd075270762f6fee4be42d92f8
makiaveli1 <itsmakiaveli@gmail.com>
2024-03-12 10:31:29 +0000

    change debug to true temporarily

--- quill/settings.py
+++ quill/settings.py
@@ -29,13 +29,13 @@ LOCAL_DEV=True
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-!%afc554mw#k5(11juwv-t##dd-a#!3bya$8go30(wmzqb3vt9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Only enable DEBUG if the LOCAL_DEV environment variable is set to "True"
-if os.getenv('LOCAL_DEV') == 'True':
-    DEBUG = True
+# if os.getenv('LOCAL_DEV') == 'True':
+#     DEBUG = True
 
-ALLOWED_HOSTS = ['quill-7g6f.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['quill-7g6f.onrender.com', '127.0.0.1', 'localhost', 'quill-01.fly.dev',]
 USE_CRISPY_BOOTSTRAP4 = True
 
 # Application definition
